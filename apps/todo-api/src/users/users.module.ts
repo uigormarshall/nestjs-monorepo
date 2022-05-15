63473ce7ad265707119da02193417ad995d6eb42
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-15 16:45:52 -0300

    feat: adiciona configuracao de injecao de dependencia do UsersService

--- apps/todo-api/src/users/users.module.ts
+++ apps/todo-api/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersRepository }],
+  exports: [TypeOrmModule]
 })
-export class UsersModule {}
+export class UsersModule { }
