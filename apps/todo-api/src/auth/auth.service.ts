bb67d3c3b2f7d9c90b7c5de8ea6fa6a25cbe716f
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-16 11:14:54 -0300

    feat: adiciona AuthService

---
+++ apps/todo-api/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, id: user.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
