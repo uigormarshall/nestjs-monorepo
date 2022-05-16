2e7004b6f95b3d52e1ef8d26a8cc33532cb0d049
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-16 09:55:57 -0300

    feat: adiciona validadores no CreateUserDto

--- apps/todo-api/src/users/dto/create-user.dto.ts
+++ apps/todo-api/src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsString()
     password: string;
 }
