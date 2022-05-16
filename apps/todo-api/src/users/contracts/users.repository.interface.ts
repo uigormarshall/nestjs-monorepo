06cadb919d1b0016c0e1cb6e3d16cab008e6562d
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-16 08:45:34 -0300

    feat: adiciona metodo  update em UsersRepositoryInterface

--- apps/todo-api/src/users/contracts/users.repository.interface.ts
+++ apps/todo-api/src/users/contracts/users.repository.interface.ts
@@ -1,8 +1,10 @@
 import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
 import { User } from "../entities/user.entity";
 
 export interface UsersRepositoryInterface {
     insert(createUserDto: CreateUserDto): Promise<User>;
+    update(id, updateUserDto: UpdateUserDto): Promise<User>;
     findAll(): Promise<User[]>
     findOne(id: string): Promise<User>
     remove(id: string): Promise<void>
