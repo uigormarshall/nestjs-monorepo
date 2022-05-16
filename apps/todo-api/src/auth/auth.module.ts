8c47605b1646102e5ccd6c5cc1a388c1a41c5faa
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-16 11:16:03 -0300

    feat: configura modulos com providers de auth

---
+++ apps/todo-api/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { UsersRepository } from '../users/users.repository';
+import { UsersService } from '../users/users.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+
+@Module({
+  imports: [UsersModule, PassportModule,   JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  }),],
+  providers: [AuthService, UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersRepository }, LocalStrategy, JwtStrategy],
+  controllers: [AuthController]
+})
+export class AuthModule {}
