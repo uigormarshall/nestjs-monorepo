ba04757ca2a8b9b23ca9bbe99721a0ad71de4958
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-17 13:58:17 -0300

    test: Create - Deve criar um User dado um CreateUserDto válido

--- apps/todo-api/src/users/users.service.spec.ts
+++ apps/todo-api/src/users/users.service.spec.ts
@@ -1,19 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersMockRepository } from '../shared/mocks/users.mock.repository';
+import { TestUtils } from '../shared/utils/test.utils';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
-
+  let repository: UsersMockRepository
   beforeEach(async () => {
+    repository = new UsersMockRepository();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersMockRepository }],
+      providers: [UsersService, { provide: 'UsersRepositoryInterface', useValue: repository }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    
   });
 
   it('UsersService deve ser definido', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Create', () => {
+    it('Deve criar um User dado um CreateUserDto válido', async () => {
+
+        const userDtoValid = TestUtils.getAValidCreateUserDto();
+        const userValid = TestUtils.getAValidUser(userDtoValid);
+        jest.spyOn(repository, 'hasRegisterWithThisEmail').mockImplementation(async () => false);
+        jest.spyOn(repository, 'insert').mockImplementation(async () => userValid);
+        const result = await service.create(userDtoValid)
+        expect(result).toEqual(userValid);
+    })
+  })
 });
