a78a6b065ff96c0b60a555a29dc57da5ad9fa54e
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-17 12:31:37 -0300

    feat: adiciona camada basica de logger

--- apps/todo-api/src/main.ts
+++ apps/todo-api/src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { BasicLoggerService } from './shared/loggers/basic.logger.service';
 import { TodoApiModule } from './todo-api.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TodoApiModule);
+  const app = await NestFactory.create(TodoApiModule,{
+    logger: new BasicLoggerService(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
