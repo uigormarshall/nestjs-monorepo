bc6f73d3dccb0af0820e59ec0634cf8bfe9c2046
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-17 12:51:22 -0300

    test: corrige teste UsersService deve ser definido

--- apps/todo-api/src/users/users.service.spec.ts
+++ apps/todo-api/src/users/users.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersMockRepository } from '../shared/mocks/users.mock.repository';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -6,13 +7,13 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersMockRepository }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
+  it('UsersService deve ser definido', () => {
     expect(service).toBeDefined();
   });
 });
