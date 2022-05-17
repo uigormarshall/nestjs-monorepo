2c112ccd5d630731e76097ac84fc009dfa87f3de
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-17 14:58:34 -0300

    feat: adiciona tipos de retorno dos metodos de UsersService

--- apps/todo-api/src/users/users.service.ts
+++ apps/todo-api/src/users/users.service.ts
@@ -2,27 +2,28 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { UsersRepositoryInterface } from './contracts/users.repository.interface';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(@Inject('UsersRepositoryInterface') private usersRepository: UsersRepositoryInterface){
 
   }
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) : Promise<User> {
     const hasUserWithEmail = await this.usersRepository.hasRegisterWithThisEmail(createUserDto.email);
     if(hasUserWithEmail) throw new HttpException('Ops! Já existe um usuario com esse email.', HttpStatus.BAD_REQUEST);
     return this.usersRepository.insert(createUserDto)
   }
 
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.usersRepository.findAll();
   }
 
-  findOne(id: string) {
+  findOne(id: string) : Promise<User>{
     return this.usersRepository.findOne(id);
   }
 
-  findOneByEmail(email: string) {
+  findOneByEmail(email: string) : Promise<User>{
     return this.usersRepository.findOneByEmail(email);
   }
 
@@ -32,7 +33,7 @@ export class UsersService {
     return this.usersRepository.update(id, updateUserDto);
   }
 
-  remove(id: string) {
+  remove(id: string) : Promise<void> {
     return this.usersRepository.remove(id);
   }
 }
