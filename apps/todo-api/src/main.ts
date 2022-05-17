c0f95f0f581e8b1c89a806907bdeac3fa97d1e6c
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-17 16:27:47 -0300

    feat: adiciona configuracao basica swagger

--- apps/todo-api/src/main.ts
+++ apps/todo-api/src/main.ts
@@ -2,12 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { BasicLoggerService } from './shared/loggers/basic.logger.service';
 import { TodoApiModule } from './todo-api.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(TodoApiModule,{
     logger: new BasicLoggerService(),
   });
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle('Todo-API')
+  .setDescription('Api Utilizada para fins didaticos')
+  .setVersion('1.0')
+  .addTag('TodoApi')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
