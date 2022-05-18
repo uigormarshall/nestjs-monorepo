5db9cb3bb2a0599800ad49a0f15e5288444ffb27
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-18 17:29:15 -0300

    fix: remove uso de guard local

--- apps/todo-api/src/auth/auth.controller.ts
+++ apps/todo-api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
@@ -8,9 +9,9 @@ import { LocalAuthGuard } from './local-auth.guard';
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @UseGuards(LocalAuthGuard)
+
     @Post('auth/login')
-    async login(@Request() req) {
-      return this.authService.login(req.user);
+    async login(@Body() loginDto: LoginDto ) {
+      return this.authService.login(loginDto);
     }
 }
