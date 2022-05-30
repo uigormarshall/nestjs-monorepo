7f1815817ffe64b0daf60d9325f760896bd0ea38
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-05-30 14:11:11 -0300

    hotfix: remove uso de local.strateg y por falta de utilizacao

--- apps/todo-api/src/auth/auth.module.ts
+++ apps/todo-api/src/auth/auth.module.ts
@@ -8,14 +8,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
+
 
 @Module({
   imports: [UsersModule, PassportModule,   JwtModule.register({
     secret: jwtConstants.secret,
     signOptions: { expiresIn: '86400s' },
   }),],
-  providers: [AuthService, UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersRepository }, LocalStrategy, JwtStrategy],
+  providers: [AuthService, UsersService, { provide: 'UsersRepositoryInterface', useClass: UsersRepository }, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
